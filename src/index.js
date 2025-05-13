7137141caed88e26890d169c6c90124e3deb6d62
Andreas Alexander <andreasalexander@Andreass-MacBook-Air.local>
2025-05-13 22:06:30 +0700

    fix: add vercel configurations

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', async (req, res, next) => {
   return res.status(200).send({ msg: 'Hello World' });
